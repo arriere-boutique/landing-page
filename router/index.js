622dee97c511d4474d8fe1ae66ef1d807547a5c9
Théotime Colin <contact@theotimecolin.com>
2022-01-25 23:06:39 +0100

    fix(Admin): fix

--- router/index.js
+++ router/index.js
@@ -17,7 +17,7 @@ export function createRouter(ssrContext, createDefaultRouter, routerOptions) {
     routesDirectory = matcher[1] || matcher[0];
     // if the subdomain is not in the list of user provided domains, set the rootdirectory to root - domain given by the user
     
-    if (routesDirectory != 'arriere-boutique') {
+    if (routesDirectory != 'arriere-boutique' && routesDirectory != 'www') {
       let found = subdomains.find(s => s.sub == routesDirectory)
       routesDirectory = found ? found.directory : '';
     } else {
